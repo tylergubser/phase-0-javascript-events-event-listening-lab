8cbdda81bf58d11e4a2c80f722c06a7a9cdabafe
tylergubser <tylergubser2200@gmail.com>
2022-02-07 16:19:32 -0600

    commit tree 75befe458c2ff0f38ed494710fb493f82e577379

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
+    input.addEventListener('click', addingEventListener);
+    return alert('I was clicked!');
+    
 }
+
+
